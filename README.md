b67426cbc118bb901dea92a3ac4b0597e50dca50
Jochen Gojowsky <jochen.gojowsky@iconparc.de>
2025-10-16 13:35:01 +0200

    Initial commit with torn attack last action userscript

---
+++ README.md
@@ -0,0 +1,1 @@
+# Torn Attack Last Action
---
+++ torn-attack-last-action.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Torn Attack Last Action
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Last action enhancements for Torn City
+// @author       xlemmingx [2035104]
+// @match        https://www.torn.com/loader.php*
+// @grant        none
+// @run-at       document-end
+// @updateURL    https://github.com/xlemmingx/torn-attack-last-action/raw/main/torn-attack-last-action.user.js
+// @downloadURL  https://github.com/xlemmingx/torn-attack-last-action/raw/main/torn-attack-last-action.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Script functionality will be implemented here
+
+})();
